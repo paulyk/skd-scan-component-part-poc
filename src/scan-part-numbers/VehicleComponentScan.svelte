8ae19c5defb8b198cd4615702cd03c51311b2e7c
Paul Krisadaolarn <pauly.k@gmail.com>
2020-09-18 11:38:18 +0700

    cleanup

--- src/scan-part-numbers/VehicleComponentScan.svelte
+++ src/scan-part-numbers/VehicleComponentScan.svelte
@@ -1,84 +1,80 @@
 <script>
-	export let vehicle = {}
-	import { createEventDispatcher } from 'svelte'
+  export let vehicle = {};
+  import { debounce } from "./util";
+  import { createEventDispatcher } from "svelte";
 
-	let dispatch = createEventDispatcher()
-	let eventName = 'vehicle-component'
-	
-	let componentCode = ""
-	let vehicleComponent = null
-	
-	$: {
+  let dispatch = createEventDispatcher();
+  let eventName = "vehicle-component";
 
-	}
+  let componentCode = "";
+  let vehicleComponent = null;
 
-	function getComponent() {
-		vehicleComponent = vehicle.vehicleComponents.find(vc => vc.component.code.toLowerCase() === componentCode.toLowerCase())
-		dispatch(eventName, vehicleComponent)
-	}
-	
-	function reset() {
-		componentCode = null
-		vehicleComponent = null
-		dispatch(eventName, null)
-	}
-	
+  $: {
+  }
+
+  function getComponent() {
+    vehicleComponent = vehicle.vehicleComponents.find(
+      (vc) => vc.component.code.toLowerCase() === componentCode.toLowerCase()
+    );
+    dispatch(eventName, vehicleComponent);
+  }
+
+  function reset() {
+    componentCode = null;
+    vehicleComponent = null;
+    dispatch(eventName, null);
+  }
 </script>
 
 <style>
-	.two-col {
-		display: grid;
-		grid-template-columns: 1fr 60px;
+  .two-col {
+    display: grid;
+    grid-template-columns: 1fr 60px;
+  }
 
-	}
-	
-	.two-col .reset {
-		padding: 0 1rem;
-		display: flex;
-		align-items: center; 
-	}
-	
-	.form {
-		padding: 0.5rem 1rem;
-		background: #fff;
-	}
-	input {
-		width: 100%;
-	}
-	
-	.detail {
-		padding: 1rem;
-		background: #eee;
-	}
-	
-	.title {
-		display: flex;
-		justify-content: center;
-		padding: 0.5rem;
-	}
+  .two-col .reset {
+    padding: 0 1rem;
+    display: flex;
+    align-items: center;
+  }
+
+  .form {
+    padding: 0.5rem 1rem;
+    background: #fff;
+  }
+  input {
+    width: 100%;
+  }
+
+  .detail {
+    padding: 1rem;
+    background: #eee;
+  }
+
+  .title {
+    display: flex;
+    justify-content: center;
+    padding: 0.5rem;
+  }
 </style>
 
 <div class="two-col">
-	
-	<div class="form">
-		<div class="title">
-			 Vehicle Component 
-		</div>
-			
-		{#if !vehicleComponent}
-			<input type=text bind:value={componentCode} on:keyup={getComponent} />
+  <div class="form">
+    <div class="title">Vehicle Component</div>
+
+    {#if !vehicleComponent}
+      <input
+        type="text"
+        bind:value={componentCode}
+        on:keyup={debounce(getComponent, 500)} />
     {:else}
-			<div class="detail">
-			  <div>{vehicleComponent.component.code}</div>	
-				<div>{vehicleComponent.component.name}</div>	 
-			</div>
-		{/if}
-	</div>
-	<div class="reset">
-		{#if vehicleComponent}
-		<button on:click={reset}>
-			Reset
-		</button>
-		{/if}
-	</div>
+      <div class="detail">
+        <div>{vehicleComponent.component.code}</div>
+        <div>{vehicleComponent.component.name}</div>
+      </div>
+    {/if}
+  </div>
+  <div class="reset">
+    {#if vehicleComponent}<button on:click={reset}> Reset </button>{/if}
+  </div>
 </div>
