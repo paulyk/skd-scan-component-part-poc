53a648b23f800c93843ec03c839be6fdf341d9a9
Paul Krisadaolarn <pauly.k@gmail.com>
2020-09-18 21:57:19 +0700

    vehicleList working with apollo-boost

--- src/components/dashboard/VehicleList.svelte
+++ src/components/dashboard/VehicleList.svelte
@@ -1,4 +1,48 @@
 <script lang="ts">
+  import { getClient } from "../../skd-data";
+  import type { ApolloQueryResult } from "apollo-boost";
+  import type {
+    GetVehiclesQuery,
+    GetVehiclesQueryVariables,
+  } from "../../generated/graphql";
+  import { getVehicles } from "../../graphql/query";
 
+  let client = getClient();
+  type LoadingState = "idle" | "pending" | "fulfilled" | "error";
+  let result: ApolloQueryResult<GetVehiclesQuery> = null;
+
+  interface State {
+    loading: LoadingState;
+    result?: ApolloQueryResult<GetVehiclesQuery>;
+    error: any;
+  }
+
+  const state: State = {
+    loading: "idle",
+    result: null,
+    error: null,
+  };
+
+  load();
+
+  async function load() {
+    console.log("loading");
+    state.loading = "pending";
+    try {
+      state.result = await client.query<GetVehiclesQuery, GetVehiclesQueryVariables>({
+        query: getVehicles,
+      });
+      state.loading = 'fulfilled'
+      console.log("loaded", state.result);
+    } catch (err) {
+      state.error = err;
+    }
+  }
 </script>
+
 <h2>Vehicles</h2>
+{#if state.loading === 'pending'}
+  loading...
+{:else if state.loading === 'fulfilled'}
+  <pre>{JSON.stringify(state.result, null, 2)}</pre>
+{:else if state.loading === 'error'}error{/if}
